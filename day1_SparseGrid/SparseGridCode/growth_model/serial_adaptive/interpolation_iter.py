e80c8c317fee8a9503710cf3bf25c1542b42ec5b
Reiko Laski <reiko.laski@gmail.com>
2018-07-11 19:47:03 -0500

    upload

--- day1_SparseGrid/SparseGridCode/growth_model/serial_adaptive/interpolation_iter.py
+++ day1_SparseGrid/SparseGridCode/growth_model/serial_adaptive/interpolation_iter.py
@@ -34,27 +34,41 @@ def sparse_grid_iter(n_agents, iDepth, valold):
     grid.makeLocalPolynomialGrid(iDim, iOut, iDepth, which_basis, "localp")
     grid.setDomainTransform(ranges)
     
-    file=open("comparison1.txt", 'w')
+    # file=open("comparison1.txt", 'w')
     # for iK in range(refinement_level):
     #     if iK == 0:
     #         aPoints=grid.getPoints()
     #     else:
+    #         grid.setSurplusRefinement(fTol, 1, "fds")
+    #         aPoints=grid.getNeededPoints()
+    #     iNumP1=aPoints.shape[0]
+    #     aVals=np.empty([iNumP1, 1])
+    #     for iI in range(iNumP1):
+    #         aVals[iI]=solveriter.iterate(aPoints[iI], n_agents, valold)[0]
+    #         v=aVals[iI]*np.ones((1,1))
+    #         to_print=np.hstack((aPoints[iI].reshape(1,n_agents), v))
+    #         np.savetxt(file, to_print, fmt='%2.16f')
+    #     valold = grid
+    #     grid.loadNeededPoints(aVals)
+
+    # file.close()
+    
     grid = valold
     grid.setSurplusRefinement(fTol, 1, "fds")
     aPoints=grid.getNeededPoints()
     iNumP1=aPoints.shape[0]
     aVals=np.empty([iNumP1, 1])
+
+    file=open("comparison1.txt", 'w')
     for iI in range(iNumP1):
         aVals[iI]=solveriter.iterate(aPoints[iI], n_agents, valold)[0]
         v=aVals[iI]*np.ones((1,1))
         to_print=np.hstack((aPoints[iI].reshape(1,n_agents), v))
         np.savetxt(file, to_print, fmt='%2.16f')
-        valold = grid
-        grid.loadNeededPoints(aVals)
-
+        
     file.close()
-    
-    
+    grid.loadNeededPoints(aVals)
+
     f=open("grid_iter.txt", 'w')
     np.savetxt(f, aPoints, fmt='% 2.16f')
     f.close()
