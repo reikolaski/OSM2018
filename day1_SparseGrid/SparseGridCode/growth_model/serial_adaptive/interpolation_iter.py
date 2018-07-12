9e445f34f550cf51da3e01c17fff1995f346ea65
Reiko Laski <reiko.laski@gmail.com>
2018-07-11 20:38:36 -0500

    upload

--- day1_SparseGrid/SparseGridCode/growth_model/serial_adaptive/interpolation_iter.py
+++ day1_SparseGrid/SparseGridCode/growth_model/serial_adaptive/interpolation_iter.py
@@ -20,21 +20,19 @@ def sparse_grid_iter(n_agents, iDepth, valold):
     
     grid  = TasmanianSG.TasmanianSparseGrid()
 
-    k_range=np.array([k_bar, k_up])
+    # k_range=np.array([k_bar, k_up])
 
-    ranges=np.empty((n_agents, 2))
+    # ranges=np.empty((n_agents, 2))
 
 
-    for i in range(n_agents):
-        ranges[i]=k_range
+    # for i in range(n_agents):
+    #     ranges[i]=k_range
 
-    iDim=n_agents
+    # iDim=n_agents
 
-    grid.makeLocalPolynomialGrid(iDim, iOut, iDepth, which_basis, "localp")
-    grid.setDomainTransform(ranges)
-
+    # grid.makeLocalPolynomialGrid(iDim, iOut, iDepth, which_basis, "localp")
+    # grid.setDomainTransform(ranges)
     grid=valold
-
     grid.setSurplusRefinement(fTol, 1, "fds")
     aPoints=grid.getNeededPoints()
     iNumP1=aPoints.shape[0]
