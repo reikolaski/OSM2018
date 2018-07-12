47a5bcc6683a29aa7a575acec9a4a73c273e0b4a
Reiko Laski <reiko.laski@gmail.com>
2018-07-11 19:22:23 -0500

    upload

--- day1_SparseGrid/SparseGridCode/growth_model/serial_adaptive/parameters.py
+++ day1_SparseGrid/SparseGridCode/growth_model/serial_adaptive/parameters.py
@@ -22,7 +22,7 @@ refinement_level = 5
 
 # control of iterations
 numstart = 0   # which is iteration to start (numstart = 0: start from scratch, number=/0: restart)
-numits = 1    # which is the iteration to end
+numits = 10    # which is the iteration to end
 
 # How many random points for computing the errors
 No_samples = 1000
