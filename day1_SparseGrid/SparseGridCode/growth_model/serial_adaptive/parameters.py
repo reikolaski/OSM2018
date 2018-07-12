cb08b2920c4778912f3f5d3ace24922a88b36ca2
Reiko Laski <reiko.laski@gmail.com>
2018-07-11 20:57:00 -0500

    upload

--- day1_SparseGrid/SparseGridCode/growth_model/serial_adaptive/parameters.py
+++ day1_SparseGrid/SparseGridCode/growth_model/serial_adaptive/parameters.py
@@ -18,11 +18,11 @@ iDepth=1
 iOut=1         # how many outputs
 which_basis = 1 #linear basis function (2: quadratic local basis)
 fTol = 1.E-5
-refinement_level = 5
+refinement_level = 3
 
 # control of iterations
 numstart = 0   # which is iteration to start (numstart = 0: start from scratch, number=/0: restart)
-numits = 10    # which is the iteration to end
+numits = 3    # which is the iteration to end
 
 # How many random points for computing the errors
 No_samples = 1000
